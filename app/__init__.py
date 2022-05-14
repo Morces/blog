cce63097b5d080bda8de0ab95a6b099e5e29ea2d
Moses Karani <karanim594@gmail.com>
2022-05-14 20:28:58 +0300

    optimizzed configurations and initialized them

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,19 @@
 from flask import Flask 
 from flask_bootstrap import Bootstrap
+from config import config_options
 
 
 
 bootstrap = Bootstrap()
 
-def create_app():
+def create_app(config_name):
+
     app = Flask(__name__)
+    app.config.from_object(config_options[config_name])
 
 
 
-
-    # Initializinf flask extensions
+    # Initializing flask extensions
     bootstrap.init_app(app)
 
 
---
+++ config.py
@@ -0,0 +1,16 @@
+import  os
+
+
+class Config():
+    pass
+
+class DevConfig(Config):
+    DEBUG = True
+
+class ProdConfig(Config):
+    pass
+
+config_options = {
+    'development': DevConfig,
+    'production' : ProdConfig
+}
