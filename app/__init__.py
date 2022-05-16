5e7ae08d4b9142c89e8dfebe962937739e8ded84
Moses Karani <karanim594@gmail.com>
2022-05-16 13:23:14 +0300

    added a login_manager

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,17 @@
+from flask_bcrypt import Bcrypt
 from flask import Flask 
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from config import config_options
 
 
-
+bcrypt= Bcrypt()
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.session_protection='strong'
+login_manager.login_view='auth.login'
 
 def create_app(config_name):
 
@@ -25,5 +30,7 @@ def create_app(config_name):
     # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
+    bcrypt.init_app(app)
 
     return app 
--- app/models.py
+++ app/models.py
@@ -1,7 +1,12 @@
 from datetime import datetime
 from flask_login import UserMixin
 from werkzeug.security import check_password_hash, generate_password_hash
-from app import db
+from . import db, login_manager
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 class User(db.Model, UserMixin):
